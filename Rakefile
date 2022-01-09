133ed7dca07f03e0c3a31be23a2b4cf55d9b7df0
Dimitri Arrigoni <dimitri@arrigoni.me>
2022-01-09 22:13:29 +0100

    feat(rake): introduce Rakefile

---
+++ Rakefile
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'sys/proc'
+Sys::Proc.progname = nil
+
+# main ----------------------------------------------------------------
+require_relative './dev/rake'
---
+++ dev/rake/tasks.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'kamaze/project'
+
+# noinspection RubyLiteralArrayInspection
+[
+  'cs:correct',
+  'cs:control',
+  'cs:pre-commit',
+  'doc',
+  'doc:watch',
+  'gem:gemspec',
+  'misc:gitignore',
+  'shell',
+  'sources:license',
+].then do |tasks|
+  Kamaze.project do |project|
+    project.subject = Kernel
+    project.name = 'rqb'
+    project.tasks = tasks
+  end.load!
+end
+
+# default task --------------------------------------------------------
+task default: ['gem:gemspec']
+
+# tasks ---------------------------------------------------------------
+Dir.glob("#{__dir__}/tasks/**/*.rb").sort.each { |fp| require(fp) }
