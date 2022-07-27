70ea6b8648c59a506a3764070c7ddb17e8e0072a
Dimitri Arrigoni <dimitri@arrigoni.me>
2022-07-27 19:36:14 +0200

    feat(subcommands): intrdouce BaseCommand#tmpdir method

--- lib/rqb/cli/base/base_command.rb
+++ lib/rqb/cli/base/base_command.rb
@@ -6,11 +6,28 @@ require_relative '../app'
 #
 # @abstract
 class Rqb::Cli::Base::BaseCommand < Rqb::Cli::Command
+  autoload(:Pathname, 'pathname')
+
   class << self
     protected
 
     def inherited(subclass, &block)
       super(subclass).then { block&.call(subclass) }
+    end
+  end
+
+  protected
+
+  # Returns the operating system's temporary file path.
+  #
+  # @return [Pathname]
+  def tmpdir
+    lambda do
+      require 'tmpdir'
+
+      Dir.tmpdir
+    end.yield_self do |tmp|
+      Pathname.new(ENV.fetch('TMPDIR', tmp.call)).expand_path
     end
   end
 end
